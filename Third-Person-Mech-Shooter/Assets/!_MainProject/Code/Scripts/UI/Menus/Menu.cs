e4994931bca23142f0234288e0a4e3d5de1defe6
L-Swiftsong <acarey300@caledonian.ac.uk>
2026-03-09 16:52:43 +0000

    Menu.cs Comments Expansion
    We've added more to the comments within Menu.cs and have made the ones that were supposed to be xml tags into them.

--- Third-Person-Mech-Shooter/Assets/!_MainProject/Code/Scripts/UI/Menus/Menu.cs
+++ Third-Person-Mech-Shooter/Assets/!_MainProject/Code/Scripts/UI/Menus/Menu.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using Cysharp.Threading.Tasks;
+﻿using Cysharp.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Gameplay.UI.Menus
 {
+    /// <summary>
+    ///     Base class for any Menu: UI elements that can be opened and closed where one takes input priority over others.
+    /// </summary>
     public class Menu : MonoBehaviour
     {
         [field: SerializeField] protected CanvasGroup CanvasGroup { get; private set; }
@@ -21,7 +22,13 @@ namespace Gameplay.UI.Menus
         }
 
 
-        // Open this menu.
+        /// <summary>
+        ///     Open this menu.
+        /// </summary>
+        /// <param name="selectFirstElement">
+        ///     If true, then we select our <see cref="FirstSelectedElement"/> when opened, if there is one.<br/>
+        ///     Otherwise, the selected GameObject is untouched.
+        /// </param>
         public virtual void Open(bool selectFirstElement = true)
         {
             Debug.Log(this.name + " Select First?: " + selectFirstElement);
@@ -30,13 +37,20 @@ namespace Gameplay.UI.Menus
             if (selectFirstElement)
                 EventSystem.current.SetSelectedGameObject(FirstSelectedElement);
         }
+        /// <summary>
+        ///     Re-opens this menu.<br/>
+        ///     Should be called from the MenuManager when returning to this menu from a menu open above it (Created more recently).
+        /// </summary>
+        /// <param name="targetSelectable"> The Selectable that we should set as the selected gameobject. If null, defaults to <see cref="FirstSelectedElement"/></param>
         public virtual void Reopen(Selectable targetSelectable = null)
         {
             Show();
             EventSystem.current.SetSelectedGameObject(targetSelectable != null ? targetSelectable.gameObject : FirstSelectedElement);
         }
 #pragma warning disable CS1998 // The base implementation of this method is synchronus, but children may not be. Suppress the warning: "Async method lacks 'await' operators and will run synchronously".
-        // Close this menu.
+        /// <summary>
+        ///     Close this menu.
+        /// </summary>
         public virtual async UniTask<bool> Close()
 #pragma warning restore CS1998 
         {
@@ -46,9 +60,13 @@ namespace Gameplay.UI.Menus
         }
 
 
-        // Shows the menu without performing the rest of its opening logic.
+        /// <summary>
+        ///     Shows the menu without performing the rest of its opening logic.
+        /// </summary>
         public virtual void Show() => CanvasGroup.Show();
-        // Hides the menu without performing the rest of its closing logic.
+        /// <summary>
+        ///     Hides the menu without performing the rest of its closing logic.
+        /// </summary>
         public virtual void Hide() => CanvasGroup.Hide();
     }
 }
