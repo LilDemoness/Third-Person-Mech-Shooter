6cb060ff9ee22b16c00b91a2c835e220cdfa0d83
LilDemoness <were.t.rex11@gmail.com>
2026-04-21 18:43:58 +0100

    Vent Heat basic vfx
    We've created a basic visual for the Vent Heat action, along with implementing the ability for the SpecialFXActionVisual to be parented to the owner and have an offset from the origin (Used to have the effect travel with the player and be positioned at their chest rather than their feet)

--- Third-Person-Mech-Shooter/Assets/!_MainProject/Code/Scripts/Actions/ActionVisuals/SpecialFXActionVisual.cs
+++ Third-Person-Mech-Shooter/Assets/!_MainProject/Code/Scripts/Actions/ActionVisuals/SpecialFXActionVisual.cs
@@ -12,19 +12,37 @@ namespace Gameplay.Actions.Visuals
     {
         [SerializeField] private SpecialFXGraphic _specialFXGraphic;
 
+        [SerializeField] private bool _parentToOriginator = false;
 
-        protected override void Trigger(ClientCharacter clientCharacter, in Vector3 origin, in Vector3 direction) => ActivateSpecialFXGraphic(origin, direction);
+        [Space(5)]
+        [SerializeField] private Vector3 _originOffset = Vector3.zero;
+        [SerializeField] private bool _localOffset = false;
+
+
+        protected override void Trigger(ClientCharacter clientCharacter, in Vector3 origin, in Vector3 direction) => ActivateSpecialFXGraphic(clientCharacter, origin, direction);
         
 
 
-        private void ActivateSpecialFXGraphic(in Vector3 origin, in Vector3 direction)
+        private void ActivateSpecialFXGraphic(ClientCharacter clientCharacter, in Vector3 origin, in Vector3 direction)
         {
             if (_specialFXGraphic == null)
                 return;
 
             SpecialFXGraphic specialFXGraphic = SpecialFXPoolManager.GetFromPrefab(_specialFXGraphic);
+            if (_parentToOriginator)
+                specialFXGraphic.transform.SetParent(clientCharacter.ServerCharacter.Movement.RotationPivot);
+
             specialFXGraphic.transform.position = origin;
             specialFXGraphic.transform.rotation = Quaternion.LookRotation(direction);
+
+            if (_originOffset != Vector3.zero)
+            {
+                if (_localOffset)
+                    specialFXGraphic.transform.localPosition += _originOffset;
+                else    
+                    specialFXGraphic.transform.position += _originOffset;
+            }
+
 
             specialFXGraphic.OnShutdownComplete += SpecialFXGraphic_OnShutdownComplete;
 
