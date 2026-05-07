7d919cc40a708e08f10ce1a632bb39f7511c1ed3
LilDemoness <were.t.rex11@gmail.com>
2026-05-07 12:26:25 +0100

    Lobby Property Display Improvement
    We've improved how the Lobby List UI Item displays information, making the Game Mode use their Display Name and formatting the map name from the scene value to a display value
    
    Additionally, we've changed to using SmallCaps rather than ForcingToUppercase, allowing us to see variations in capitalisation within the room name or properties

--- Third-Person-Mech-Shooter/Assets/!_MainProject/Code/Scripts/UI/Menus/MainMenu/Sessions/LobbyListItemUI.cs
+++ Third-Person-Mech-Shooter/Assets/!_MainProject/Code/Scripts/UI/Menus/MainMenu/Sessions/LobbyListItemUI.cs
@@ -31,19 +31,58 @@ namespace Gameplay.UI.Menus.Session
 
             if (data.Properties != null)
             {
-                if (data.Properties.TryGetValue("GameMode", out SessionProperty gameModeProperty))
-                    _lobbyGameModeText.SetText(gameModeProperty.Value);
-                else
-                    _lobbyGameModeText.SetText("Error");
-
-                if (data.Properties.TryGetValue("Map", out SessionProperty mapProperty))
-                    _lobbyMapText.SetText(mapProperty.Value);
-                else
-                    _lobbyMapText.SetText("Error");
+                _lobbyGameModeText.SetText(GetGameModeString(data));
+                _lobbyMapText.SetText(GetMapNameString(data));
             }
 
             _passwordProtectedCheckGO.SetActive(data.HasPassword);
         }
+
+        private string GetGameModeString(ISessionInfo data)
+        {
+            const string ERROR_VALUE = "Error";
+            if (!data.Properties.TryGetValue("GameMode", out SessionProperty gameModeProperty))
+                return ERROR_VALUE;
+            if (!System.Enum.TryParse<GameMode>(gameModeProperty.Value, out GameMode gameMode))
+                return ERROR_VALUE;
+
+            return gameMode.ToDisplayName();
+        }
+        private string GetMapNameString(ISessionInfo data)
+        {
+            const string ERROR_VALUE = "Error";
+
+            if (!data.Properties.TryGetValue("Map", out SessionProperty mapProperty))
+                return ERROR_VALUE;
+
+            // Generate a display string for the map name, treating Uppercase Characters followed by Lowercase Characters as the start of a new word.
+            // E.g.
+            //  ASimpleTest -> A Simple Test
+            //  OrAnFPSCheck -> Or An FPS Check
+            //  TestingA -> Testing A
+            string displayString = mapProperty.Value[0].ToString();
+            for(int i = 1; i < mapProperty.Value.Length - 1; ++i)
+            {
+                if (char.IsUpper(mapProperty.Value[i]) && !char.IsUpper(mapProperty.Value[i + 1]))
+                {
+                    // This character symbolises the start of a new word as it is uppercase while the next is lowercase.
+                    // Add a space before inserting this character.
+                    displayString += ' ';
+                }
+                
+                displayString += mapProperty.Value[i];
+            }
+
+            // Add the final character. If that character is uppercase but the previous wasn't, then treat it as the start of a new word.
+            if (mapProperty.Value.Length > 1 && char.IsUpper(mapProperty.Value[mapProperty.Value.Length - 1]) && !char.IsUpper(mapProperty.Value[mapProperty.Value.Length - 2]))
+                displayString += ' ';
+            displayString += mapProperty.Value[mapProperty.Value.Length - 1];
+
+
+            // Return our map name display string.
+            return displayString;
+        }
+
 
         public void OnClick()
         {
